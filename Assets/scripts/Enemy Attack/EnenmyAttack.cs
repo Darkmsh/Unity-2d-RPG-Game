be82b871f8192fceda528202190035eb99224bcf
DarkMSH <darkmohammadgamer@gmail.com>
2025-11-11 15:04:04 +0330

    Chasing Animation added and now enemy can attack to player

--- Assets/scripts/Enemy Attack/EnenmyAttack.cs
+++ Assets/scripts/Enemy Attack/EnenmyAttack.cs
@@ -7,18 +7,23 @@ public class EnenmyAttack : MonoBehaviour
     public Transform player;
     public LayerMask playerLayer;
     public Transform detectionPoint;
+    public Transform attackPoint;
     public float findRange;
+    public float attackRange;
     public float speed;
 
     private Transform enemyPosition;
     private Rigidbody2D rb;
     private AnimationStiuation enemySitiuation;
+    private Animator anim;
 
     void Start()
     {
         enemyPosition = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 
+        changAnimation(AnimationStiuation.Idle);
 
     }
 
@@ -38,18 +43,60 @@ public class EnenmyAttack : MonoBehaviour
         else
         {
             rb.linearVelocity = Vector2.zero;
+            changAnimation(AnimationStiuation.Idle);
         }
     }
     void chacing()
     {
-        Vector2 space = player.position - enemyPosition.position;
-        rb.linearVelocity=space*speed;  
+        changAnimation(AnimationStiuation.Chasing);
+        Vector2 direction = player.position - enemyPosition.position;
+        rb.linearVelocity = direction * speed;
+        Attacking();
+    }
+    void changAnimation(AnimationStiuation enemyAnim)
+    {
+        if (enemySitiuation == AnimationStiuation.Idle)
+        {
+            anim.SetBool("isidle", false);
+        }
+        else if (enemySitiuation == AnimationStiuation.Chasing)
+        {
+            anim.SetBool("ischasing", false);
+        }
+        else if (enemySitiuation == AnimationStiuation.Attacking)
+        {
+            anim.SetBool("isattacking", false);
+        }
+
+        enemySitiuation = enemyAnim;
+
+        if (enemySitiuation == AnimationStiuation.Idle)
+        {
+            anim.SetBool("isidle", true);
+        }
+        else if (enemySitiuation == AnimationStiuation.Chasing)
+        {
+            anim.SetBool("ischasing", true);
+        }
+        else if (enemySitiuation == AnimationStiuation.Attacking)
+        {
+            anim.SetBool("isattacking", true);
+        }
+    }
+    void Attacking()
+    {
+        Collider2D[] attack = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, playerLayer);
+        if (attack.Length > 0)
+        {
+            changAnimation(AnimationStiuation.Attacking);
+        }
+        
     }
 }
 
 enum AnimationStiuation
 {
     Idle,
-    Walking,
+    Chasing,
     Attacking,
 }
